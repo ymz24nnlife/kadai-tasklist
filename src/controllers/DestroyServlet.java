acb18f6f94b9fc7f2b42a1dbe419b9cc43794877
shoya.yamasaki <shoya.yamasaki.nnlife@outlook.jp>
2023-05-19 14:30:45 +0900

    DestroyServlet

---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String _token = request.getParameter("_token");
+		if(_token != null && _token.contentEquals(request.getSession().getId())) {
+			EntityManager em = DBUtil.createEntityManager();
+
+			//データ取得
+			Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+			em.getTransaction().begin();
+			em.remove(t);
+			em.getTransaction().commit();
+			request.getSession().setAttribute("flush", "登録が完了しました。");
+			em.close();
+
+			//不要データ削除
+			request.getSession().removeAttribute("task_id");
+
+			//redirect
+			response.sendRedirect(request.getContextPath() + "/index");
+		}
+	}
+
+}
